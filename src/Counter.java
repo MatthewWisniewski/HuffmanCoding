dcd49cda024ab1b17be112f46c12cb5021f55eb7
Clevershelf009 <matthewski490@yahoo.co.uk>
2019-12-26 23:04:53 +0000

    Create class to count how many times each character appears in a string

---
+++ src/Counter.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class Counter {
+
+    public static HashMap<Character, Integer> countCharacters (String str) {
+        HashMap<Character, Integer> characterCounts = new HashMap<>();
+        for (Character c : str.toCharArray()) {
+            if (characterCounts.containsKey(c)) {
+                characterCounts.put(c, characterCounts.get(c) + 1);
+            } else {
+                characterCounts.put(c, 1);
+            }
+        }
+        return characterCounts;
+    }
+
+}
